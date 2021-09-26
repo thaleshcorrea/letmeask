27c5c219a56ad8a1e89514133e4366424f468b25
Thales <thaleshenrique.correa@gmail.com>
2021-09-26 16:53:44 -0300

    Add home screen and rooms

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,22 @@
-import { Button } from './components/Button'
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from "./contexts/AuthContext";
+import { Room } from "./pages/Room";
 
 function App() {
   return (
-    <div>
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-      <Button />
-    </div>
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/rooms/new" component={NewRoom} />
+          <Route path="/rooms/:id" component={Room} />
+        </Switch>
+      </BrowserRouter>
+    </AuthContextProvider>
   );
 }
 
